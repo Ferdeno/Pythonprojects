64878c547725fe6043aa923fb2129b5e1a94d56d
Ferdeno <125113134+Ferdeno@users.noreply.github.com>
2023-07-18 23:57:50 +0530

    Add files via upload

---
+++ Day 2 Project Tip Calculator/Exercise 3 - Life in Weeks.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+days=str((90-int(age))*365)
+weeks=str((90-int(age))*52)
+month=str((90-int(age))*12)
+print(f"You have {days} days, {weeks} weeks, and {month} months left.")
+
+
